77b850db190eaf3e4d34a3e13e21504d20612a5d
Massimiliano Bisceglie <maxbisceglie89@gmail.com>
2024-02-02 16:07:02 +0000

    website done

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,28 @@ module.exports = {
     title: `Cerberus Capital Property Investments`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-postcss"]
+  plugins: [
+    "gatsby-plugin-postcss",
+    {
+      resolve: `gatsby-omni-font-loader`,
+      options: {
+        enableListener: true,
+        preconnect: [`https://fonts.googleapis.com`, `https://fonts.gstatic.com`],
+        web: [
+          {
+            name: `Open Sans`,
+            file: `https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap`,
+          },
+          {
+            name: `Roboto`,
+            file: `https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap`,
+          },
+          {
+            name: `Cambria`,
+            file: `https://fonts.googleapis.com/css2?family=Cambria:wght@400;700&display=swap`,
+          }
+        ],
+      },
+    },
+  ]
 };
---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <nav className='flex py-10'>
+      <h3>
+        Logo
+      </h3>
+    </nav>
+  );
+}
+
+export default Navbar;
